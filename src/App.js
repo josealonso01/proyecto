60f7c25bf2b390b1266a8925825905e5de383efa
Josefina Alonso <joseealonsoo01@gmail.com>
2022-04-21 23:37:21 -0300

    agregue components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import { CartWidget } from './components/CartWidget';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>BIENVENIDOS</h1>
-        <h2>-By: Josefina Alonso-</h2>
-        <a
-          className="App-link"
-          href="https://github.com/josealonso01"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click aqui para acceder a mi repositorio 
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <ItemListContainer greeting='BEBE A BORDO WEBSITE'></ItemListContainer>
+    </>
   );
 }
 export default App;
